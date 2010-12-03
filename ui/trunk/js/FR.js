1c8f3d7626630c07f0be2c2a455b9a80234a370c
jkar <jkar@967a2e56-9f69-44db-a86b-3acc15697453>
2010-12-03 12:40:40 +0000

    rooms are rendered
    git-svn-id: file:///srv/svn/repos/futurice_reservator@18 967a2e56-9f69-44db-a86b-3acc15697453

--- ui/trunk/js/FR.js
+++ ui/trunk/js/FR.js
@@ -1,14 +1,13 @@
 var FR = function(){
     var rooms = [
 		{floor : 4, name : 'Panorama', number : 401, location : [978,354,118,82] },
-		{floor : 4, name : 'Pilotti', number : 402, location : [1135,354,118,82] },
+		{floor : 4, name : 'Pilotti', number : 402, location : [1099,354,118,82] },
 		{floor : 4, name : 'Vauhtimato', number : 403, location : [1220,354,118,82] },
 		{floor : 4, name : 'Space Shot', number : 404, location : [979,184,178,129] },
-		{floor : 4, name : 'Regatta', number : 405, location : [10,354,118,85] },
-		{floor : 4, name : 'Kino', number : 406, location : [1135,56,118,85] },
-		{floor : 4, name : 'Vekkula', number : 407, location : [978,56,118,85] },
-		{floor : 4, name : 'Metkula', number : 408, location : [775,56,197,85] },
 		{floor : 4, name : 'Regatta', number : 405, location : [1220,56,118,85] },
+		{floor : 4, name : 'Kino', number : 406, location : [1099,56,118,85] },
+		{floor : 4, name : 'Vekkula', number : 407, location : [978,56,118,85] },
+		{floor : 4, name : 'Metkula', number : 408, location : [776,56,199,85] },
 		{floor : 4, name : 'Keittiö', number : 499, location : [10,186,142,130,10,316,480,120] },
 	];
 	
