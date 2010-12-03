c5d5457e11ef4888aedf859a53a646a2f7656c2a
ykar <ykar@967a2e56-9f69-44db-a86b-3acc15697453>
2010-12-03 13:39:31 +0000

    add mime types
    git-svn-id: file:///srv/svn/repos/futurice_reservator@31 967a2e56-9f69-44db-a86b-3acc15697453

--- backend/trunk/views/reservation.py
+++ backend/trunk/views/reservation.py
@@ -7,7 +7,13 @@ def index(request):
         return post(request)
 
 def get(request):
-    return render_to_response('get-reservation-test-data.json')
+    return render_to_response('get-reservation-test-data.json', 
+                              {}, 
+                              mimetype="application/json")
 
+# test POST with
+# curl -H "X-Requested-With: XMLHttpRequest" -X POST -D - http://dyn-2-182.lan.futurice.org:8000/reservation/
 def post(request):
-    return render_to_response('post-reservation-test-data.json')
+    return render_to_response('post-reservation-test-data.json', 
+                              {},
+                              mimetype="application/json")
