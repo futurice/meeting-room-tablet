8af0278e6721db3cb1b13f2a15f3c48780e1348c
mmul <mmul@967a2e56-9f69-44db-a86b-3acc15697453>
2010-12-03 12:42:09 +0000

    git-svn-id: file:///srv/svn/repos/futurice_reservator@19 967a2e56-9f69-44db-a86b-3acc15697453

---
+++ backend/trunk/views/reservation.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+def index(request):
+    if request.method == 'GET':
+        return get(request)
+    elif request.method == 'POST':
+        return post(request)
+
+def get(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
+
+def get(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
