7a6c9a921e14bfea5b5c2ac3ab3ce32625650cc5
vman <vman@967a2e56-9f69-44db-a86b-3acc15697453>
2011-06-03 07:18:33 +0000

    git-svn-id: file:///srv/svn/repos/futurice_reservator@39 967a2e56-9f69-44db-a86b-3acc15697453

---
+++ android/Reservator/src/com/futurice/android/reservator/Reservator.java
@@ -0,0 +1,13 @@
+package com.futurice.android.reservator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Reservator extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
