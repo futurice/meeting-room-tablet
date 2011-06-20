94169f411e3a881c57b445b4c463d1c952a532b2
ogre <ogre@967a2e56-9f69-44db-a86b-3acc15697453>
2011-06-20 05:15:22 +0000

    humanizeTimeSpan forgotten helper
    git-svn-id: file:///srv/svn/repos/futurice_reservator@76 967a2e56-9f69-44db-a86b-3acc15697453

--- android/Reservator/src/com/futurice/android/reservator/common/Helpers.java
+++ android/Reservator/src/com/futurice/android/reservator/common/Helpers.java
@@ -32,4 +32,21 @@ public class Helpers {
 			return "";
 		}
 	}
+
+	// TODO: an hour and a half - and other common expressions
+	public static String humanizeTimeSpan(int minutes) {
+		if (minutes < 30) {
+			return Integer.toString(minutes) + " minutes";
+		} else if (minutes < 45) {
+			return "half an hour";
+		} else if (minutes < 60) {
+			return "45 minutes";
+		} else if (minutes < 85) {
+			return "an hour";
+		} else if (minutes < 110) {
+			return "an hour and a half";
+		} else {
+			return Integer.toString((minutes + 10)/60) + " hours";
+		}
+	}
 }
