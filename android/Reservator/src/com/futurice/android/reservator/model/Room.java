0095bf0d845ced8f115d502227e06e1cce04efd0
ogre <ogre@967a2e56-9f69-44db-a86b-3acc15697453>
2011-06-20 05:15:23 +0000

    fixed Room.isFree, added minutesFreeFrom and reservedForFrom
    git-svn-id: file:///srv/svn/repos/futurice_reservator@80 967a2e56-9f69-44db-a86b-3acc15697453

--- android/Reservator/src/com/futurice/android/reservator/model/Room.java
+++ android/Reservator/src/com/futurice/android/reservator/model/Room.java
@@ -3,7 +3,6 @@ package com.futurice.android.reservator.model;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 public class Room {
@@ -17,7 +16,7 @@ public class Room {
 		this.dataProxy = dataProxy;
 		this.reservations = new ArrayList<Reservation>();
 	}
-	
+
 	public List<Reservation> getReservations(boolean forceRefresh) throws ReservatorException {
 		if (forceRefresh || this.reservations == null) {
 			reservations = dataProxy.getRoomReservations(this);
@@ -32,7 +31,7 @@ public class Room {
 	public void addReservation(Reservation r) {
 		// TODO: tell data proxy
 		// should this take start, begin time and reservator's name as parameters?
-		
+
 		this.reservations.add(r);
 		Collections.sort(this.reservations);
 	}
@@ -51,18 +50,61 @@ public class Room {
 	}
 
 	public boolean isFree() {
-		Date now = new Date();
+		Calendar now = Calendar.getInstance();
 		for (Reservation r : reservations) {
-			if (r.getBeginTime().before(now)) {
-				if (r.getEndTime().after(now)) {
-					return false;
-				}
-			} else {
-				return true;
+			if (r.getBeginTime().before(now) && r.getEndTime().after(now)) {
+				return false;
 			}
 		}
+
 		return true;
 	}
+
+	/**
+	 * Time in minutes the room is free
+	 * Precondition: room is free
+	 *
+	 * @param from
+	 * @return Integer.MAX_VALUE if no reservations in future
+	 */
+	public int minutesFreeFrom(Calendar from) {
+		for (Reservation r : reservations) {
+			if (r.getBeginTime().after(from)) {
+				return (int) (r.getBeginTime().getTimeInMillis() - from.getTimeInMillis()) / 60000;
+			}
+		}
+
+		return Integer.MAX_VALUE;
+	}
+
+	public int minutesFreeFromNow() {
+		return minutesFreeFrom(Calendar.getInstance());
+	}
+
+
+	/**
+	 * Time in minutes room is reserved
+	 * Precondition: room is reserved
+	 *
+	 * @param from
+	 * @return
+	 */
+	public int reservedForFrom(Calendar from) {
+		Calendar to = (Calendar) from.clone();
+		for (Reservation r : reservations) {
+			if (r.getBeginTime().before(to) && r.getEndTime().after(to)) {
+				to = (Calendar) r.getEndTime().clone();
+				to.add(Calendar.MINUTE, 5);
+			}
+		}
+
+		return (int) (to.getTimeInMillis() - from.getTimeInMillis()) / 60000;
+	}
+
+	public int reservedForFromNow() {
+		return reservedForFrom(Calendar.getInstance());
+	}
+
 	public Reservation getNextFreeTime(){
 		Calendar now = Calendar.getInstance();
 		for (Reservation r : reservations) {
