5a92ec68a83a5ee71ad88b806a2a751dfc25e883
ogre <ogre@967a2e56-9f69-44db-a86b-3acc15697453>
2011-06-20 05:15:23 +0000

    Configurable (JSON) room infos
    git-svn-id: file:///srv/svn/repos/futurice_reservator@79 967a2e56-9f69-44db-a86b-3acc15697453

--- android/Reservator/src/com/futurice/android/reservator/model/rooms/RoomsInfo.java
+++ android/Reservator/src/com/futurice/android/reservator/model/rooms/RoomsInfo.java
@@ -1,24 +1,82 @@
 package com.futurice.android.reservator.model.rooms;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.util.Log;
+
+import com.futurice.android.reservator.common.Helpers;
 import com.futurice.android.reservator.model.Room;
 
 public class RoomsInfo {
 	private int roomSize;
+	private int roomNumber;
+	private String roomName;
 
-	private RoomsInfo(int roomSize) {
+	private static Map<String, RoomsInfo> rooms = null;
+
+	private RoomsInfo(String roomName, int roomNumber, int roomSize) {
 		this.roomSize = roomSize;
+		this.roomNumber = roomNumber;
+		this.roomName = roomName;
 	}
 
-	// TODO: just a stub
-	public static RoomsInfo getRoomsInfo(Room room) {
-		if (room.getName().contains("404")) {
-			return new RoomsInfo(42);
-		} else {
-			return new RoomsInfo(6);
-		}
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
 	}
 
 	public int getRoomSize() {
 		return roomSize;
 	}
+
+	@SuppressWarnings("unchecked")
+	private static void loadRooms() {
+		rooms = new HashMap<String, RoomsInfo>();
+
+		try {
+			JSONObject object = (JSONObject) new JSONTokener(Helpers.readFromInputStream(RoomsInfo.class.getResourceAsStream("rooms.json"), -1)).nextValue();
+			Iterator<String> iter = object.keys();
+			while (iter.hasNext()) {
+				String key = iter.next();
+				try {
+					JSONObject room = object.getJSONObject(key);
+
+					// default size of six is better than nothing
+					rooms.put(key.toLowerCase(), new RoomsInfo(room.getString("name"), room.optInt("number"), room.optInt("size", 6)));
+				} catch (JSONException e) {
+					// do nothing
+				}
+			}
+		} catch (JSONException e) {
+			// do nothing
+		}
+	}
+
+	// TODO: just a stub
+	public static RoomsInfo getRoomsInfo(Room room) {
+		if (rooms == null) loadRooms();
+
+		String email = room.getEmail();
+		int atindex = email.indexOf('@');
+
+		RoomsInfo info;
+
+		Log.v("roomsinfo", email.substring(0, atindex));
+
+		if (atindex == -1 || (info = rooms.get(email.substring(0, atindex).toLowerCase())) == null) {
+			return new RoomsInfo(room.getName(), 0, 6);
+		} else {
+			return info;
+		}
+	}
+
 }
