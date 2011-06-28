737f7c3c79e3f815fb1e170f19abaa7b1aa44d8e
ogre <ogre@967a2e56-9f69-44db-a86b-3acc15697453>
2011-06-28 13:59:47 +0000

    add more verbose messages in exceptions thrown by timespan
    git-svn-id: file:///srv/svn/repos/futurice_reservator@138 967a2e56-9f69-44db-a86b-3acc15697453

--- android/Reservator/src/com/futurice/android/reservator/model/TimeSpan.java
+++ android/Reservator/src/com/futurice/android/reservator/model/TimeSpan.java
@@ -7,12 +7,12 @@ public class TimeSpan {
 		this.start = start;
 		this.end = end;
 		if(start.after(end)){
-			throw new IllegalArgumentException("No negative time allowed");
+			throw new IllegalArgumentException("No negative time allowed -- " + start.toGMTString() + " - " + end.toGMTString());
 		}
 	}
 	public TimeSpan(DateTime start, int units, int count){
 		if(count < 0){
-			throw new IllegalArgumentException("No negative time allowed");
+			throw new IllegalArgumentException("No negative time allowed -- count: " + count);
 		}
 		if(start == null){
 			start = new DateTime();
@@ -30,7 +30,7 @@ public class TimeSpan {
 
 		this.start = start;
 		if(start.after(end)){
-			throw new IllegalArgumentException("No negative time allowed");
+			throw new IllegalArgumentException("No negative time allowed -- setStart");
 		}
 	}
 	public final DateTime getEnd() {
@@ -39,7 +39,7 @@ public class TimeSpan {
 	public void setEnd(DateTime end) {
 		this.end = end;
 		if(start.after(end)){
-			throw new IllegalArgumentException("No negative time allowed");
+			throw new IllegalArgumentException("No negative time allowed -- setEnd");
 		}
 	}
 	public long getLength(){
