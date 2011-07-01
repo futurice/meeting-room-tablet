e738c29adb7531133a2f8622ba26a6131f626fa1
vman <vman@967a2e56-9f69-44db-a86b-3acc15697453>
2011-07-01 14:42:21 +0000

    javadoc for ReservatorVisualizer
    git-svn-id: file:///srv/svn/repos/futurice_reservator@167 967a2e56-9f69-44db-a86b-3acc15697453

--- android/Reservator/src/com/futurice/android/reservator/view/ReservatorVisualizer.java
+++ android/Reservator/src/com/futurice/android/reservator/view/ReservatorVisualizer.java
@@ -5,9 +5,26 @@ import java.util.List;
 import com.futurice.android.reservator.model.DateTime;
 import com.futurice.android.reservator.model.Reservation;
 import com.futurice.android.reservator.model.TimeSpan;
-
+/**
+ * Interface for visualizing reservation data. getSelectedTime and getSelectedTimeSpan can be called after onClick to get
+ * DateTime and TimeSpan associated with the click event 
+ * @author vman
+ */
 public interface ReservatorVisualizer {
+	
+	/**
+	 * Set reservations. The reservation references should be copied to an internal array. 
+	 * @param reservations
+	 */
 	public void setReservations(List<Reservation> reservations);
+	/**
+	 * Get selected DateTime after onClick
+	 * @return
+	 */
 	public DateTime getSelectedTime();
+	/**
+	 * Get selected TimeSpan after onClick
+	 * @return
+	 */
 	public TimeSpan getSelectedTimeSpan();
 }
