9030a62c35e1fdf1482501f7379c4e409927e20c
Ville Sinisalo <ville.sinisalo@futurice.com>
2013-06-20 18:30:27 +0300

    remove unused classes (old Exchange API)

--- android/Reservator/src/com/futurice/android/reservator/model/soap/EWS/MicrosoftStyle.java
+++
@@ -1,9 +0,0 @@
-package com.futurice.android.reservator.model.soap.EWS;
-
-import org.simpleframework.xml.stream.CamelCaseStyle;
-
-public class MicrosoftStyle extends CamelCaseStyle {
-	public String getAttribute(String name) {
-		return getElement(name);
-	}
-}
