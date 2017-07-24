a132794d6b3fccf47cf2e19dcb6c8d796ebae7e7
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-07-24 12:29:34 +0300

    Feature/check permission (#51)
    * Adds Activity to check permissions and gitignore
    
    * Configs xml files and gradle properties
    
    * Changes code indentation to 4 tabs
    
    * Only request permissions on API > 23
    
    * Ignore .idea files

--- app/src/main/java/com/futurice/android/reservator/LoginActivity.java
+++ app/src/main/java/com/futurice/android/reservator/LoginActivity.java
@@ -1,126 +1,92 @@
 package com.futurice.android.reservator;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-
 import com.futurice.android.reservator.common.PreferenceManager;
 import com.futurice.android.reservator.model.AddressBook;
 import com.futurice.android.reservator.model.AddressBookUpdatedListener;
 import com.futurice.android.reservator.model.ReservatorException;
 
 public class LoginActivity extends ReservatorActivity
-        implements AddressBookUpdatedListener {
+    implements AddressBookUpdatedListener {
 
-    private boolean addressBookOk = false;
-    private boolean roomListOk = false;
+  private boolean addressBookOk = false;
+  private boolean roomListOk = false;
 
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-
-        if (!PreferenceManager.getInstance(this)
-                .getApplicationConfigured()) {
-            showWizard();
-            return;
-        }
-
-        setContentView(R.layout.login_activity);
-
-        if (!havePermissions) {
-            havePermissions = checkPermissions();
-        }
-        if (havePermissions) {
-            // Check Google Calendar
-            checkCalendarAndFetchEntries();
-        }
+    if (!PreferenceManager.getInstance(this)
+        .getApplicationConfigured()) {
+      showWizard();
+      return;
     }
 
-    private void checkCalendarAndFetchEntries() {
-        if (getResApplication().getDataProxy().hasFatalError()) {
-            showWizard();
-            return;
-        } else {
-            roomListOk = true;
-        }
+    setContentView(R.layout.login_activity);
+
+    checkCalendarAndFetchEntries();
+  }
+
+  private void checkCalendarAndFetchEntries() {
+    if (getResApplication().getDataProxy().hasFatalError()) {
+      showWizard();
+      return;
+    } else {
+      roomListOk = true;
     }
+  }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+  @Override
+  public void onResume() {
+    super.onResume();
 
-        AddressBook ab = this.getResApplication().getAddressBook();
-        ab.addDataUpdatedListener(this);
-        ab.refetchEntries();
-        checkAndGo();
+    AddressBook ab = this.getResApplication().getAddressBook();
+    ab.addDataUpdatedListener(this);
+    ab.refetchEntries();
+    checkAndGo();
+  }
+
+  public void onPause() {
+    super.onPause();
+
+    AddressBook ab = this.getResApplication().getAddressBook();
+    ab.removeDataUpdatedListener(this);
+  }
+
+  private void checkAndGo() {
+    if (addressBookOk && roomListOk) {
+
+      final Intent i = new Intent(this, LobbyActivity.class);
+      startActivity(i);
     }
+  }
 
-    public void onPause() {
-        super.onPause();
+  @Override
+  public void addressBookUpdated() {
+    addressBookOk = true;
+    checkAndGo();
+  }
 
-        AddressBook ab = this.getResApplication().getAddressBook();
-        ab.removeDataUpdatedListener(this);
-    }
+  @Override
+  public void addressBookUpdateFailed(ReservatorException e) {
+    addressBookOk = false;
+    // return to config
+    showWizard();
+  }
 
+  private void showWizard() {
+    final Intent i = new Intent(this, WizardActivity.class);
+    startActivity(i);
+  }
 
-    private void checkAndGo() {
-        if (addressBookOk && roomListOk) {
-
-            final Intent i = new Intent(this, LobbyActivity.class);
-            startActivity(i);
-        }
-    }
-
-    @Override
-    public void addressBookUpdated() {
-        addressBookOk = true;
-        checkAndGo();
-    }
-
-    @Override
-    public void addressBookUpdateFailed(ReservatorException e) {
-        addressBookOk = false;
-        // return to config
-        showWizard();
-    }
-
-    private void showWizard() {
-        final Intent i = new Intent(this, WizardActivity.class);
-        startActivity(i);
-    }
-
-    @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
-        finish();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode,
-            String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions,
-                                         grantResults);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST: {
-                if (grantResults.length >= 3
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
-                    grantResults[2] == PackageManager.PERMISSION_GRANTED) {
-                    havePermissions = true;
-                    checkCalendarAndFetchEntries();
-                } else {
-                    finish();
-                }
-                return;
-            }
-
-
-        }
-    }
+  @Override
+  protected void onActivityResult(
+      int requestCode, int resultCode, Intent data) {
+    finish();
+  }
 }
 
--- app/src/main/java/com/futurice/android/reservator/ReservatorActivity.java
+++ app/src/main/java/com/futurice/android/reservator/ReservatorActivity.java
@@ -1,15 +1,9 @@
 package com.futurice.android.reservator;
 
-import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -22,18 +16,12 @@ public class ReservatorActivity extends Activity {
 
     private final ReservatorAppHandler handler = new ReservatorAppHandler();
     private GoToFavouriteRoom goToFavouriteRoomRunable;
-    public final int PERMISSIONS_REQUEST = 234;
     protected boolean havePermissions = false;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        if (!havePermissions) {
-            havePermissions = checkPermissions();
-        }
-        if (havePermissions) {
-            goToFavouriteRoomRunable = new GoToFavouriteRoom(this);
-        }
+        goToFavouriteRoomRunable = new GoToFavouriteRoom(this);
     }
 
     public void onResume() {
@@ -52,57 +40,6 @@ public class ReservatorActivity extends Activity {
         startAutoGoToFavouriteRoom();
     }
 
-    public boolean checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                                              Manifest.permission.READ_CONTACTS)
-            != PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(this,
-                                              Manifest.permission.READ_CALENDAR)
-            != PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(this,
-                                              Manifest.permission.WRITE_CALENDAR)
-            != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                                                                    Manifest.permission.READ_CONTACTS) ||
-                ActivityCompat.shouldShowRequestPermissionRationale(this,
-                                                                    Manifest.permission.READ_CALENDAR) ||
-                ActivityCompat.shouldShowRequestPermissionRationale(this,
-                                                                    Manifest.permission.WRITE_CALENDAR)) {
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.permission_request_title)
-                        .setMessage(R.string.permission_request_reason)
-                        .setPositiveButton("Ok",
-                                           new DialogInterface.OnClickListener() {
-                                               @Override public void onClick(
-                                                       DialogInterface dialog,
-                                                       int which) {
-                                                   ActivityCompat.requestPermissions(ReservatorActivity.this, new String[]{
-                                                           Manifest.permission.READ_CONTACTS,
-                                                           Manifest.permission.READ_CALENDAR,
-                                                           Manifest.permission.WRITE_CALENDAR
-                                                   }, PERMISSIONS_REQUEST);
-                                               }
-                                           })
-                        .setNegativeButton("Dismiss",  new DialogInterface.OnClickListener() {
-                            @Override public void onClick(
-                                    DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                finish();
-                            }
-                        })
-                        .show();
-            } else {
-                ActivityCompat.requestPermissions(this,
-                                                  new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALENDAR,
-                                                          Manifest.permission.WRITE_CALENDAR},
-                                                  PERMISSIONS_REQUEST);
-
-            }
-            return false;
-        } else {
-            return true;
-        }
-    }
 
     /**
      * @return Identical to getApplication, but returns a ReservatorApplication.
@@ -159,34 +96,13 @@ public class ReservatorActivity extends Activity {
                     room = activity.getResApplication().getDataProxy().getRoomWithName(roomName);
                 } catch (ReservatorException ex) {
                     Toast err = Toast.makeText(activity, ex.getMessage(),
-                        Toast.LENGTH_LONG);
+                            Toast.LENGTH_LONG);
                     err.show();
                     return;
                 }
                 RoomActivity.startWith(activity, room);
             }
             activity.onPrehended();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST: {
-                if (grantResults.length >= 3
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
-                    grantResults[2] == PackageManager.PERMISSION_GRANTED) {
-                    havePermissions = true;
-                    goToFavouriteRoomRunable = new GoToFavouriteRoom(this);
-                } else {
-                    finish();
-                }
-                return;
-            }
-
-
         }
     }
 
